bcc4d80eb07fc2d670d2392a0dcb1eeb05720d0d
Milo <dudals2963@tukorea.ac.kr>
2025-11-26 15:02:19 +0900

    [Silver I] Title: 숨바꼭질, Time: 180 ms, Memory: 17016 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -11,46 +11,31 @@ let [N, K] = fs
 
 const maxLange = 200000;
 const dis = Array.from({ length: maxLange }, () => -1);
-const dx = [-1, 1, 2];
 
 const queue = [];
 let head = 0;
 let tail = 0;
 
-let isOver = 0;
-let movedPos;
 function sol(n, k) {
   let res = -1;
   queue[tail++] = n;
   dis[n] = 0;
+
   while (tail - head !== 0) {
     const curPos = queue[head++];
     const curDis = dis[curPos];
+
+    const movedPosArr = [curPos + 1, curPos - 1, curPos * 2];
     if (curPos === k) {
       res = curDis;
       break;
     }
-    isOver = 100000 <= curPos ? 1 : 0;
 
-    if (isOver) {
-      movedPos = curPos - 1;
-      if (movedPos < 0 || maxLange < movedPos) continue;
+    for (const movedPos of movedPosArr) {
+      if (movedPos < 0 || 100000 < movedPos) continue;
       if (dis[movedPos] !== -1) continue;
       queue[tail++] = movedPos;
-      dis[movedPos] = curDis + 1;
-    } else {
-      for (let i = 0; i < 3; i++) {
-        if (i === 2) {
-          movedPos = curPos * dx[i];
-        } else {
-          movedPos = curPos + dx[i];
-        }
-
-        if (movedPos < 0 || maxLange < movedPos) continue;
-        if (dis[movedPos] !== -1) continue;
-        queue[tail++] = movedPos;
-        dis[movedPos] = curDis + 1; //오타 조심......
-      }
+      dis[movedPos] = curDis + 1; //오타 조심......
     }
   }
 
