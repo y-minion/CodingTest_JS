73d506aec788e0369d7671177a24831d880d0150
Milo <dudals2963@tukorea.ac.kr>
2025-11-27 15:32:41 +0900

    [Gold V] Title: 토마토, Time: 808 ms, Memory: 170716 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.js
+++ 백준/Gold/7569. 토마토/토마토.js
@@ -5,37 +5,41 @@ const [M, N, H] = input
   .split(" ")
   .map((el) => Number(el));
 
-const board = input.map((str) => str.split(" ").map((el) => Number(el)));
-
 let totalCount = 0; //목표 토마토 개수
 let perfectCount = 0;
 let curCount = 0; //현재 토마토 개수
+
+const dx = [0, -1, 0, 1, 0, 0];
+const dy = [1, 0, -1, 0, 0, 0];
+const dz = [0, 0, 0, 0, 1, -1];
+
+//3차원 vis 배열
+const vis = Array.from({ length: H }, () =>
+  Array.from({ length: N }, () => Array.from({ length: M }, () => -1))
+);
+
+input = input.map((str) => str.split(" ").map((el) => Number(el)));
+
+const board = [];
+for (let i = 0; i < H; i++) {
+  board.push(input.slice(i * N, i * N + N));
+}
 
 const queue = [];
 let head = 0;
 let tail = 0;
 
-const dx = [0, -1, 0, 1];
-const dy = [1, 0, -1, 0];
-const dz = [N, -N]; //+N,-N
-
-//vis만들기
-const vis = Array.from({ length: N * H }, () =>
-  Array.from({ length: M }, () => -1)
-);
-
-//현재 존재하는 토마토 개수 수집
-for (let i = 0; i < N * H; i++) {
-  for (let j = 0; j < M; j++) {
-    //토마토 발견하면 개수 카운팅 + 익어있는 토마도 발견시 즉시 큐에 삽입 -> 동시 확산 -------->이때 큐에 삽입시 방문처리 필수
-    if (board[i][j] === 0) totalCount++;
-    if (board[i][j] === 1) {
-      queue[tail++] = [i, j];
-      vis[i][j] = 0; //방문 처리
-
-      curCount++; //큐에 넣을때 현재 개수도 카운팅 해줘야한다.
-      totalCount++;
-      perfectCount++;
+for (let z = 0; z < H; z++) {
+  for (let x = 0; x < N; x++) {
+    for (let y = 0; y < M; y++) {
+      if (board[z][x][y] === 0) totalCount++;
+      if (board[z][x][y] === 1) {
+        queue[tail++] = [z, x, y];
+        vis[z][x][y] = 0;
+        curCount++;
+        totalCount++;
+        perfectCount++;
+      }
     }
   }
 }
@@ -44,34 +48,28 @@ function sol() {
   if (perfectCount === totalCount) return 0;
   let curDay = 0;
   while (tail - head !== 0) {
-    const [curRow, curCol] = queue[head++];
-    const originRow = curRow % N;
-    curDay = vis[curRow][curCol];
-
-    for (let i = 0; i < 4; i++) {
-      let movedRow = originRow + dx[i];
-      let movedRealRow = curRow + dx[i];
+    const [curH, curRow, curCol] = queue[head++];
+    curDay = vis[curH][curRow][curCol];
+    for (let i = 0; i < 6; i++) {
+      let movedRow = curRow + dx[i];
       let movedCol = curCol + dy[i];
-      if (movedRow < 0 || N <= movedRow || movedCol < 0 || M <= movedCol)
-        continue;
-
+      let movedH = curH + dz[i];
       if (
-        vis[movedRealRow][movedCol] !== -1 ||
-        board[movedRealRow][movedCol] === -1
+        movedRow < 0 ||
+        N <= movedRow ||
+        movedCol < 0 ||
+        M <= movedCol ||
+        movedH < 0 ||
+        H <= movedH
       )
         continue;
-
-      queue[tail++] = [movedRealRow, movedCol];
-      vis[movedRealRow][movedCol] = curDay + 1;
-      curCount++;
-    }
-
-    for (let i = 0; i < 2; i++) {
-      const movedZ = curRow + dz[i];
-      if (movedZ < 0 || N * H <= movedZ) continue;
-      if (vis[movedZ][curCol] !== -1 || board[movedZ][curCol] === -1) continue;
-      queue[tail++] = [movedZ, curCol];
-      vis[movedZ][curCol] = curDay + 1;
+      if (
+        vis[movedH][movedRow][movedCol] !== -1 ||
+        board[movedH][movedRow][movedCol] === -1
+      )
+        continue;
+      queue[tail++] = [movedH, movedRow, movedCol];
+      vis[movedH][movedRow][movedCol] = curDay + 1;
       curCount++;
     }
   }
@@ -79,5 +77,4 @@ function sol() {
   const res = totalCount === curCount ? curDay : -1;
   return res;
 }
-
 console.log(sol());
