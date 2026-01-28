357eb1e11a1fb944457069cd473dedb85860781f
Milo <dudals2963@tukorea.ac.kr>
2026-01-29 00:44:52 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1164 ms, Memory: 229344 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+let input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => Number(el));
+
+const N = input.shift();
+const tmp = [];
+function merge(start, end) {
+  const mid = Math.floor((start + end) / 2);
+  let rIdx = mid;
+  let lIdx = start;
+  for (let i = start; i < end; i++) {
+    if (lIdx === mid) tmp[i] = input[rIdx++];
+    else if (rIdx === end) tmp[i] = input[lIdx++];
+    else if (input[rIdx] >= input[lIdx]) tmp[i] = input[rIdx++];
+    else tmp[i] = input[lIdx++];
+  }
+
+  for (let i = start; i < end; i++) {
+    input[i] = tmp[i];
+  }
+}
+
+function mergeSort(start, end) {
+  if (end - start <= 1) return;
+  const mid = Math.floor((start + end) / 2);
+
+  mergeSort(start, mid);
+  mergeSort(mid, end);
+
+  merge(start, end);
+}
+
+mergeSort(0, N);
+console.log(input.join("\n"));
