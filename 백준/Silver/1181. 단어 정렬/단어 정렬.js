ec93182371c2e34b5c0f9afaab226e3d0d4a8805
Milo <dudals2963@tukorea.ac.kr>
2026-01-29 18:22:20 +0900

    [Silver V] Title: 단어 정렬, Time: 200 ms, Memory: 18248 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -3,24 +3,17 @@ const fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().trim().split(/\s+/);
 
 const n = input.shift();
+input = [...new Set(input)];
 
 function sol() {
   input.sort((a, b) => {
     if (a.length > b.length) return 1;
     if (a.length < b.length) return -1;
-    const e = a.localeCompare(b);
-    if (e === -1) return -1;
-    if (e === 1) return 1;
-    return 0;
+    if (a < b) return -1;
+    if (a > b) return 1;
   });
 
-  const arr = [];
-  input.forEach((char) => {
-    if (arr.length !== 0 && arr[arr.length - 1] === char) return;
-    arr.push(char);
-  });
-
-  return arr.join("\n");
+  return input.join("\n");
 }
 
 const res = sol();
