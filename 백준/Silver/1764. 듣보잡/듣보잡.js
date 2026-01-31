e37af5cb0dfa63041cfe3e6ebc0f69830856a50a
Milo <dudals2963@tukorea.ac.kr>
2026-01-31 14:04:53 +0900

    [Silver IV] Title: 듣보잡, Time: 192 ms, Memory: 29640 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.js
+++ 백준/Silver/1764. 듣보잡/듣보잡.js
@@ -1,3 +1,16 @@
+/**
+ * 명단에서 듣보잡을 찾자.
+
+이름이 중복(2번)으로 나오는 순간 듣보잡이 된다.
+
+매 루프마다 항상 Map에 사람의 등록를 확인한다.
+이때 이미 등록이 되어 있으면 새로운 배열에 추가한다.
+그리고 등록한다.
+
+마지막에 배열을 정렬한다.
+
+ */
+
 const fs = require("fs");
 
 let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
